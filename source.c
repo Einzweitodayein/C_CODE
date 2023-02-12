21744fb0f308d069b6bdc5c20496227cdf217288
ein <hello@qq.com>
2023-02-12 18:36:37 +0800

    const修饰指针变量的两个位置

--- source.c
+++ source.c
@@ -2,57 +2,26 @@
 #include<stdio.h>
 #include<assert.h>
 
-//void my_strcpy(char* dest, char* src)  //构造自己的字符串拷贝函数
-//{
-//	while (*src != '\0')
-//	{
-//		*dest = *src;
-//		src++;
-//		dest++;
-//	}
-//	*dest = *src;  //'\0'
-//}
-
-//优化
-//void my_strcpy(char* dest, char* src)
-//{
-//	if (dest != NULL && src != NULL)  //防止传参时传的是NULL
-//	{
-//		while (*dest++ = *src++) //利用后置++的特性和'\0'的ASCII值为0
-//		{
-//			;
-//		}
-//	}
-//}
-
-//再优化
-//void my_strcpy(char* dest, char* src)
-//{
-//	assert(dest != NULL); //当括号内表达式为假时抛异常
-//	assert(src != NULL);
-//	while (*dest++ = *src++) //利用后置++的特性和'\0'的ASCII值为0
-//	{
-//		;
-//	}
-//}
-
-//再再优化
-void my_strcpy(char* dest, const char* src)
-{
-	assert(dest != NULL); //当括号内表达式为假时抛异常
-	assert(src != NULL);
-	while (*dest++ = *src++) //利用后置++的特性和'\0'的ASCII值为0
-	{
-		;
-	}
-}
 
 int main()
 {
-	char arr1[] = "#########";
-	char arr2[] = "bit";
-	my_strcpy(arr1, NULL);
-	printf("%s\n", arr1);
+	//const int num = 10;
+	//int* p = &num;
+	//*p = 20; //依然可以通过指针修改num常量,破坏了规则...
+	//printf("%d\n", num);
+
+	//const int num = 10;
+	//const int int* p = &num;
+	//*p = 20; //error const放在指针变量的*左边时，修饰的是*p,也就是说：不能通过p来改变*p(num)的值
+	//printf("%d\n", num);
+
+	const int num = 10;
+	int* const p = &num; //const放在指针变量的*左边时，修饰的是指针变量p本身，p不能被改变了
+	*p = 20;
+	int n = 100;
+	p = &n;  //不能被改变
+
+	printf("%d\n", num);
 	system("pause");
 	return 0;
 }
