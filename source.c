fd12dd1319650b7be74ff7801da4076186f3ad9b
ein <hello@qq.com>
2023-02-12 20:52:08 +0800

    判断机器是采用大端存储还是小端存储

--- source.c
+++ source.c
@@ -1,27 +1,19 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-#include<assert.h>
 
-
+//写一段代码告诉我们当前机器的字节序是什么
 int main()
 {
-	//const int num = 10;
-	//int* p = &num;
-	//*p = 20; //依然可以通过指针修改num常量,破坏了规则...
-	//printf("%d\n", num);
-
-	//const int num = 10;
-	//const int int* p = &num;
-	//*p = 20; //error const放在指针变量的*左边时，修饰的是*p,也就是说：不能通过p来改变*p(num)的值
-	//printf("%d\n", num);
-
-	const int num = 10;
-	int* const p = &num; //const放在指针变量的*左边时，修饰的是指针变量p本身，p不能被改变了
-	*p = 20;
-	int n = 100;
-	p = &n;  //不能被改变
-
-	printf("%d\n", num);
+	int a = 1;
+	char* p = (char*)&a;
+	if (*p == 1)
+	{
+		printf("小端\n");
+	}
+	else
+	{
+		printf("大端\n");
+	}
 	system("pause");
 	return 0;
 }
