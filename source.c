2442491e3989df74a20ed4f23ecb8c42fe7d0bc1
ein <hello@qq.com>
2023-02-13 19:39:43 +0800

    函数指针

--- source.c
+++ source.c
@@ -1,17 +1,22 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 
-void test(int** p)
-{}
+int Add(int x, int y)
+{
+	return x + y;
+}
 int main()
 {
-	int* ptr;
-	int** pp = &ptr;
-	test(&ptr);
-	test(pp);
-
-	int* arr[10];
-	test(arr); //指针数组也可以
+	int a = 10;
+	int b = 20;
+	
+	//&函数名和函数名都是函数的地址
+	/*
+	printf("%p\n", &Add);
+	printf("%p\n", Add);
+	*/
+	int (*pa)(int, int) = Add;
+	printf("%d\n", (*pa)(a, b)); //30
 	system("pause");
 	return 0;
 }
