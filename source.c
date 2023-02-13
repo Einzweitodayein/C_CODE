aa76d0fefb2a109968c9e8b73fc69274b7bbf0dd
ein <hello@qq.com>
2023-02-13 15:00:32 +0800

    指针数组的使用

--- source.c
+++ source.c
@@ -2,20 +2,21 @@
 #include<stdio.h>
 int main()
 {
-	int n = 9;
-	//0 00000000 00000000000000000001001  补码
-	float* pFloat = (float*)&n;
-	printf("n的值为:%d\n", n);  //9
-	//(-1)^0 * 0.00000000000000000001001 * 2^-126
-	printf("*pFloat的值为:%f\n", *pFloat);  //0.000000 以浮点数的形式拿整型的值
+	int arr1[] = { 1,2,3,4,5 };
+	int arr2[] = { 2,3,4,5,6 };
+	int arr3[] = { 3,4,5,6,7 };
 
-	*pFloat = 9.0;
-	//1001.0
-	//1.001*2^3
-	//(-1)^0*1.001*2^3
-	//0 10000010 00100000000000000000000
-	printf("n的值为:%d\n", n);  //1091567616 以整型的形式拿浮点数的值
-	printf("*pFloat的值为:%f\n", *pFloat);  //9.000000
+	int* parr[] = { arr1,arr2,arr3 };
+	int i = 0;
+	for (i = 0; i < 3; i++)
+	{
+		int j = 0;
+		for (j = 0; j < 5; j++)
+		{
+			printf("%d ", *(parr[i] + j));
+		}
+		printf("\n");
+	}
 	system("pause");
 	return 0;
 }
