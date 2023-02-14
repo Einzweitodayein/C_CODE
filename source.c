8ad69d4be3798f2e8ab6188eb6cadc29d52c8b74
ein <hello@qq.com>
2023-02-14 13:13:22 +0800

    void*类型

--- source.c
+++ source.c
@@ -1,71 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 
-void menu()
-{
-	printf("*************************\n");
-	printf("****  1.add   2.sub  ****\n");
-	printf("****  3.mul   4.div  ****\n");
-	printf("****       0.exit    ****\n");
-	printf("*************************\n");
-}
-
-int Add(int x, int y)
-{
-	return x + y;
-}
-int Sub(int x, int y)
-{
-	return x - y;
-}
-int Mul(int x, int y)
-{
-	return x * y;
-}
-int Div(int x, int y)
-{
-	return x / y;
-}
-
-void Calc(int(*pf)(int, int))
-{
-	int x = 0;
-	int y = 0;
-	printf("请输入两个操作数:>");
-	scanf("%d%d", &x, &y);
-	printf("%d\n", pf(x, y));
-}
 int main()
 {
-	int input = 0;
-	do
-	{
-		menu();
-		printf("请选择:>");
-		scanf("%d", &input);
-		switch (input)
-		{
-		case 1:
-			Calc(Add);
-			break;
-		case 2:
-			Calc(Sub);
-			break;
-		case 3:
-			Calc(Mul);
-			break;
-		case 4:
-			Calc(Div);
-			break;
-		case 0:
-			printf("退出\n");
-			break;
-		default:
-			printf("选择错误\n");
-			break;
+	int a = 10;
+	//int* pa = &a;
+	//char ch = 'w';
 
-		}
-	} while (input);
+	void* p = &a;
+	//p = &ch;
+	//*p = 0; error void*类型的指针不能进行解引用的操作，因为它不知道要访问几个字节
+	//p++;    error void*类型的指针不能进行加减整数的操作，因为它不知道要跳几个字节
 	system("pause");
 	return 0;
 }
