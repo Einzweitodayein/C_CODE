0e132946109f476611fd932b8a55bde4d44ca8cd
ein <hello@qq.com>
2023-02-14 16:22:38 +0800

    qsort实现不同类型数据排序

--- source.c
+++ source.c
@@ -1,16 +1,85 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+struct Stu
+{
+	char name[20];
+	int age;
+};
+int cmp_int(const void* e1, const void* e2)
+{
+	//比较两个整型值的函数
+	return *(int*)e1 - *(int*)e2;
+}
 
+void test1() //排整型
+{
+	int arr[10] = { 9,8,7,6,5,4,3,2,1,0 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	qsort(arr, sz, sizeof(arr[0]), cmp_int);
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+}
+
+int cmp_float(const void* e1, const void* e2)
+{
+	return (int)(*(float*)e1 - *(float*)e2);
+}
+void test2() //排浮点型
+{
+	float f[] = { 9.0,8.0,7.0,6.0,5.0,4.0 };
+	int sz = sizeof(f) / sizeof(f[0]);
+	qsort(f, sz, sizeof(f[0]), cmp_float);
+	int j = 0;
+	for (j = 0; j < sz; j++)
+	{
+		printf("%f ", f[j]);
+	}
+}
+
+int cmp_stu_by_age(const void* e1, const void* e2)
+{
+	return ((struct Stu*)e1)->age - ((struct Stu*)e2)->age;
+}
+void test3() //排结构体中的年龄
+{
+	struct Stu s[3] = { {"张三",20},{"李四",30},{"王五",10} };
+	int sz = sizeof(s) / sizeof(s[0]);
+	qsort(s, sz, sizeof(s[0]), cmp_stu_by_age);
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", (s+i)->age);
+	}
+}
+
+int cmp_stu_by_name(const void* e1, const void* e2)
+{
+	return strcmp(((struct Stu*)e1)->name, ((struct Stu*)e2)->name);
+}
+void test4() //排结构体中的名字
+{
+	struct Stu s[3] = { {"张三",20},{"李四",30},{"王五",10} };
+	int sz = sizeof(s) / sizeof(s[0]);
+	qsort(s, sz, sizeof(s[0]), cmp_stu_by_name);
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%s ", (s + i)->name);
+	}
+}
 int main()
 {
-	int a = 10;
-	//int* pa = &a;
-	//char ch = 'w';
-
-	void* p = &a;
-	//p = &ch;
-	//*p = 0; error void*类型的指针不能进行解引用的操作，因为它不知道要访问几个字节
-	//p++;    error void*类型的指针不能进行加减整数的操作，因为它不知道要跳几个字节
+	test1();
+	printf("\n");
+	test2();
+	printf("\n");
+	test3();
+	printf("\n");
+	test4();
+	printf("\n");
 	system("pause");
 	return 0;
 }
