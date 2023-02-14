85ae2fc45acc752b95a188abf47f4bea87342bb8
ein <hello@qq.com>
2023-02-14 18:46:18 +0800

    设计冒泡排序的通用性

--- source.c
+++ source.c
@@ -1,21 +1,58 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+
 struct Stu
 {
 	char name[20];
 	int age;
 };
-int cmp_int(const void* e1, const void* e2)
+
+void Swap(char* buf1, char* buf2, int width)
 {
-	//比较两个整型值的函数
+	int i = 0;
+	for (i = 0; i < width; i++)
+	{
+		char temp = *buf1;
+		*buf1 = *buf2;
+		*buf2 = temp;
+		buf1++;
+		buf2++;
+	}
+}
+//实现bubble_sort函数的程序员，他不知道未来排序的数据类型
+//也不知道待比较的两个元素的类型
+void buble_sort(void* base, int sz, int width, int (*cmp)(void* e1, void* e2))
+{
+	int i = 0;
+	//趟数
+	for (i = 0; i < sz-1; i++)
+	{
+		//每一趟比较的对数
+		int j = 0;
+		for (j = 0; j < sz - 1 - i; j++)
+		{
+			//两个元素的比较
+			if (cmp((char*)base+j*width,(char*)base+(j+1)*width) > 0) //传元素地址
+			{
+				//交换
+				Swap((char*)base+j*width,(char*)base+(j+1)*width,width);
+			}
+		}
+	}
+}
+
+
+int cmp_int(void* e1, void* e2)
+{
 	return *(int*)e1 - *(int*)e2;
 }
 
-void test1() //排整型
+void test1() //冒泡排整型
 {
-	int arr[10] = { 9,8,7,6,5,4,3,2,1,0 };
+	int arr[10] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
 	int sz = sizeof(arr) / sizeof(arr[0]);
-	qsort(arr, sz, sizeof(arr[0]), cmp_int);
+	buble_sort(arr, sz, sizeof(arr[0]), cmp_int);
+
 	int i = 0;
 	for (i = 0; i < sz; i++)
 	{
@@ -23,53 +60,38 @@ void test1() //排整型
 	}
 }
 
-int cmp_float(const void* e1, const void* e2)
-{
-	return (int)(*(float*)e1 - *(float*)e2);
-}
-void test2() //排浮点型
-{
-	float f[] = { 9.0,8.0,7.0,6.0,5.0,4.0 };
-	int sz = sizeof(f) / sizeof(f[0]);
-	qsort(f, sz, sizeof(f[0]), cmp_float);
-	int j = 0;
-	for (j = 0; j < sz; j++)
-	{
-		printf("%f ", f[j]);
-	}
-}
-
-int cmp_stu_by_age(const void* e1, const void* e2)
-{
-	return ((struct Stu*)e1)->age - ((struct Stu*)e2)->age;
-}
-void test3() //排结构体中的年龄
-{
-	struct Stu s[3] = { {"张三",20},{"李四",30},{"王五",10} };
-	int sz = sizeof(s) / sizeof(s[0]);
-	qsort(s, sz, sizeof(s[0]), cmp_stu_by_age);
-	int i = 0;
-	for (i = 0; i < sz; i++)
-	{
-		printf("%d ", (s+i)->age);
-	}
-}
-
-int cmp_stu_by_name(const void* e1, const void* e2)
+int cmp_stu_name(void* e1, void* e2)
 {
 	return strcmp(((struct Stu*)e1)->name, ((struct Stu*)e2)->name);
 }
-void test4() //排结构体中的名字
+
+int cmp_stu_age(void* e1, void* e2)
 {
-	struct Stu s[3] = { {"张三",20},{"李四",30},{"王五",10} };
+	return ((struct Stu*)e1)->age - ((struct Stu*)e2)->age;
+}
+void test2() //冒泡排结构体中的名字
+{
+	struct Stu s[3] = { {"张三",30},{"李四",40},{"王五",10} };
 	int sz = sizeof(s) / sizeof(s[0]);
-	qsort(s, sz, sizeof(s[0]), cmp_stu_by_name);
+	buble_sort(s, sz, sizeof(s[0]), cmp_stu_name);
 	int i = 0;
 	for (i = 0; i < sz; i++)
 	{
-		printf("%s ", (s + i)->name);
+		printf("%s ", (s+i)->name);
 	}
 }
+void test3() //冒牌排结构体中的年龄
+{
+	struct Stu s[3] = { {"张三",30},{"李四",40},{"王五",10} };
+	int sz = sizeof(s) / sizeof(s[0]);
+	buble_sort(s, sz, sizeof(s[0]), cmp_stu_age);
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", (s + i)->age);
+	}
+}
+
 int main()
 {
 	test1();
@@ -77,9 +99,6 @@ int main()
 	test2();
 	printf("\n");
 	test3();
-	printf("\n");
-	test4();
-	printf("\n");
 	system("pause");
 	return 0;
 }
