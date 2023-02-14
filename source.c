8699ec2aa102c1e44bdbcc282f0821a4b7ea4130
ein <hello@qq.com>
2023-02-14 10:04:39 +0800

    回调函数应用计算器

--- source.c
+++ source.c
@@ -26,35 +26,45 @@ int Div(int x, int y)
 {
 	return x / y;
 }
+
+void Calc(int(*pf)(int, int))
+{
+	int x = 0;
+	int y = 0;
+	printf("请输入两个操作数:>");
+	scanf("%d%d", &x, &y);
+	printf("%d\n", pf(x, y));
+}
 int main()
 {
 	int input = 0;
-	int x = 0;
-	int y = 0;
-	
-	//pfArr是一个函数指针数组——转移表
-	int(*pfArr[5])(int, int) = { 0,Add,Sub,Mul,Div };
 	do
 	{
 		menu();
 		printf("请选择:>");
 		scanf("%d", &input);
-		if (input >= 1 && input <= 4)
+		switch (input)
 		{
-			printf("请输入两个操作数:>");
-			scanf("%d%d", &x, &y);
-			int ret = pfArr[input](x, y);
-			printf("%d\n", ret);
-		}
-		else if (input == 0)
-		{
+		case 1:
+			Calc(Add);
+			break;
+		case 2:
+			Calc(Sub);
+			break;
+		case 3:
+			Calc(Mul);
+			break;
+		case 4:
+			Calc(Div);
+			break;
+		case 0:
 			printf("退出\n");
-		}
-		else
-		{
+			break;
+		default:
 			printf("选择错误\n");
+			break;
+
 		}
-		
 	} while (input);
 	system("pause");
 	return 0;
