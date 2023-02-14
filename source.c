5a095e7f3aabdef51b7a1000e628c5b514911fd2
ein <hello@qq.com>
2023-02-14 21:41:30 +0800

    strlen计算常量字符串

--- source.c
+++ source.c
@@ -2,14 +2,14 @@
 #include<stdio.h>
 int main()
 {
-	char arr[] = "abcdef";
-	printf("%d\n", strlen(arr)); //6
-	printf("%d\n", strlen(arr + 0));  //6
-	//printf("%d\n", strlen(*arr)); //非法访问 error
-	//printf("%d\n", strlen(arr[1])); //非法访问 error
-	printf("%d\n", strlen(&arr)); //6
-	printf("%d\n", strlen(&arr + 1)); //随机值
-	printf("%d\n", strlen(&arr[0] + 1)); //5
+	char *p = "abcdef";  //常量字符串 把a的地址放到p里了
+	printf("%d\n", strlen(p)); //4/8 计算指针变量的大小
+	printf("%d\n", strlen(p + 0)); //p+1得到的是字符b的地址 4/8
+	printf("%d\n", strlen(*p)); //1 *p就是字符串的第一个字符a
+	printf("%d\n", strlen(p[1])); //1 int arr[10]; arr[0] == *(arr+0)  p[0] == *(p+0) == 'a'
+	printf("%d\n", strlen(&p)); // 4/8 地址
+	printf("%d\n", strlen(&p + 1));// 4/8 地址
+	printf("%d\n", strlen(&p[0] + 1)); // 4/8 地址
 	system("pause");
 	return 0;
 }
