3aa3191dc691deefae23d0e97f2bd15f85d3f3a5
ein <hello@qq.com>
2023-02-15 14:35:44 +0800

    指针题完结

--- source.c
+++ source.c
@@ -4,10 +4,13 @@
 
 int main()
 {
-	char* a[] = { "work","at","alibaba" };
-	char** pa = a;
-	pa++;
-	printf("%s\n", *pa); //at
+	char* c[] = { "ENTER","NEW","POINT","FIRST" };
+	char** cp[] = { c + 3,c + 2,c + 1,c };
+	char*** cpp = cp;
+	printf("%s\n", **++cpp);
+	printf("%s\n", *-- * ++cpp + 3);  //printf()里面的表达式++运算结果保留 c上一个cpp的变化会保留
+	printf("%s\n", *cpp[-2] + 3);  //cpp[-2]==*(cpp+(-2))
+	printf("%s\n", cpp[-1][-1] + 1);
 	system("pause");
 	return 0;
 }
