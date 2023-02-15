2ef02aec70dfe9dbc0966e8ffd2377f8a6e4ee01
ein <hello@qq.com>
2023-02-15 10:48:26 +0800

    指针加减整数

--- source.c
+++ source.c
@@ -1,10 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+
+struct Test
+{
+	int Num;
+	char* pcName;
+	short sDate;
+	char cha[2];
+	short sBa[4];
+}*p;
+//假设p的值为0x100000 如下表达式的值分别为多少？
+//已知，结构体Test类型的变量大小是20个字节
 int main()
 {
-	int a[5] = { 1,2,3,4,5 };
-	int* ptr = (int*)(&a + 1);
-	printf("%d,%d\n", *(a + 1), *(ptr - 1));  // 2 5
+	p = (struct Test*)0x100000;
+	printf("%p\n", p + 0x1); //0x00100014
+	printf("%p\n", (unsigned long)p + 0x1); //0x00100001
+	printf("%p\n", (unsigned int*)p + 0x1); //0x00100004
 	system("pause");
 	return 0;
 }
+//指针+-整数取决于指针类型
