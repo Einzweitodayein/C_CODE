d861ca800d984adb64f9823c8aba6bae43b2df07
ein <hello@qq.com>
2023-02-16 13:10:06 +0800

    杨氏矩阵

--- source.c
+++ source.c
@@ -3,37 +3,50 @@
 #include<assert.h>
 #include<string.h>
 
-//写一个函数，判断一个字符串是否为另一个字符串旋转之后的字符串
-//例如给s1=AABCD和s2=BCDAA,返回1
-//给定s1=abcd和s2=ACBBD,返回0
 
-//KMP算法
-int is_left_move(char* s1, char* s2)
+//杨氏矩阵
+//有一个数字矩阵，矩阵的每行从左到右是递增的，矩阵从上到下是递增的
+//请编写程序在这样的矩阵中查找某个数字是否存在
+//要求时间复杂度小于O(N);
+
+int FindNum(int arr[3][3], int k, int row, int col)
 {
-	//1.在str1字符串中追加一个str1字符串
-	//strncat
-	int len1 = strlen(s1);
-	int len2 = strlen(s2);
-	if (len1 != len2)  //两字符串长度不相同那一定不是旋转得到
-		return 0;
-	strncat(s1, s1, len1);
-	//2.判断str2指向的字符串是否是str1指向的字符串的字串
-	//strstr-找字串的
-	char* str = strstr(s1, s2);  //在s1里找s2 找到了就返回第一个字符的地址，找不到就返回空指针
-	if (str == NULL)
-		return 0;
-	else
-		return 1;
+	int x = 0;
+	int y = col - 1;
+	while (x < row && y >= 0 )
+	{
+		if (arr[x][y] > k)
+		{
+			y--;
+
+		}
+		else if (arr[x][y] < k)
+		{
+			x++;
+		}
+		else
+		{
+			return 1;
+		}
+	}
+	//找不到
+	return 0;
 }
 int main()
 {
-	char arr1[30] = "abcdef";
-	char arr2[] = "cdefab";
-	int ret = is_left_move(arr1, arr2);
+	int arr[3][3] = { {1,2,3},
+		              {4,5,6},
+		              {7,8,9} };
+	int k = 9;
+	int ret = FindNum(arr, k,3,3);
 	if (ret == 1)
-		printf("Yes\n");
+	{
+		printf("存在\n");
+	}
 	else
-		printf("No\n");
+	{
+		printf("不存在\n");
+	}
 	system("pause");
 	return 0;
 }
