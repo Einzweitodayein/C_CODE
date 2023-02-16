1900e47df83b4caa3ae2a457210c3cf73ccfeade
ein <hello@qq.com>
2023-02-16 12:27:16 +0800

    KMP算法实现判断一个字符是否为另一个字符翻转之后的字符串

--- source.c
+++ source.c
@@ -7,37 +7,27 @@
 //例如给s1=AABCD和s2=BCDAA,返回1
 //给定s1=abcd和s2=ACBBD,返回0
 
-void left_move(char* arr,int k)
-{
-	int len = strlen(arr);
-	int i = 0;
-	for (i = 0; i < k; i++)
-	{
-		int temp = *arr;
-		int j = 0;
-		for (j = 0; j < len - 1; j++)
-		{
-			*(arr + j) = *(arr + j + 1);
-		}
-		*(arr + len - 1) = temp;
-	}
-}
+//KMP算法
 int is_left_move(char* s1, char* s2)
 {
-	int len = strlen(s1);
-	int i = 0;
-	for (i = 0; i < len; i++)
-	{
-		left_move(s1, 1);
-		int ret = strcmp(s1, s2);
-		if (ret == 0)
-			return 1;
-	}
-	return 0;
+	//1.在str1字符串中追加一个str1字符串
+	//strncat
+	int len1 = strlen(s1);
+	int len2 = strlen(s2);
+	if (len1 != len2)  //两字符串长度不相同那一定不是旋转得到
+		return 0;
+	strncat(s1, s1, len1);
+	//2.判断str2指向的字符串是否是str1指向的字符串的字串
+	//strstr-找字串的
+	char* str = strstr(s1, s2);  //在s1里找s2 找到了就返回第一个字符的地址，找不到就返回空指针
+	if (str == NULL)
+		return 0;
+	else
+		return 1;
 }
 int main()
 {
-	char arr1[] = "abcdef";
+	char arr1[30] = "abcdef";
 	char arr2[] = "cdefab";
 	int ret = is_left_move(arr1, arr2);
 	if (ret == 1)
