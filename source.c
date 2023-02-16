011c9d63835d0b065d517005e42a185b74a30e2a
ein <hello@qq.com>
2023-02-16 22:14:17 +0800

    strtok

--- source.c
+++ source.c
@@ -3,43 +3,28 @@
 #include<assert.h>
 #include<string.h>
 
-char* my_strstr(const char* p1, const char* p2)
-{
-	assert(p1 && p2);
-	char* s1 = p1;
-	char* s2 = p2;
-	char* cur = p1;
-	if (*p2 == '\0') //当要查的字符串为""时
-	{
-		return p1;
-	}
-	while (*cur)
-	{
-		s1 = cur;
-		s2 = p2;
-		while ((*s1 != '\0') && (*s2 != '\0') && (*s1 == *s2))
-		{
-			s1++;
-			s2++;
-		}
-		if (*s2 == '\0')
-		{
-			return cur; //找到子串
-		}
-		cur++;
-	}
-	return NULL;  //找不到子串
-}
 int main()
 {
-	char* p1 = "abcdef";
-	char* p2 = "bcdo";
-	char* ret = my_strstr(p1, p2);
-	if (ret == NULL)
-	{
-		printf("子串不存在\n");
-	}
-	else
+	char arr[] = "zpw@bitedu.tech";
+	char* p = "@.";
+
+	char buf[1024] = { 0 };
+	strcpy(buf, arr);
+
+	//切割buf中的字符串
+	
+	//写法不好
+	//char* ret = strtok(arr, p);
+	//printf("%s\n", ret); //zpw
+
+	//ret = strtok(NULL, p);
+	//printf("%s\n", ret); //bitedu
+
+	//ret = strtok(NULL, p);
+	//printf("%s\n", ret); //tech
+
+	char* ret = NULL;
+	for (ret = strtok(arr, p); ret != NULL; ret = strtok(NULL, p))
 	{
 		printf("%s\n", ret);
 	}
