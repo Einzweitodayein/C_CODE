e1f374accbfd2caf3278f29595e4777d840b677c
ein <hello@qq.com>
2023-02-16 09:53:23 +0800

    打印菱形

--- source.c
+++ source.c
@@ -1,16 +1,57 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+#include<assert.h>
 
 
+void Print(int k)
+{
+	int i = 0;
+	for (i = 0; i < k; i++)
+	{
+		int j = 0;
+		for (j = 0; j < k - 1-i; j++)
+		{
+			printf(" ");
+		}
+		
+		for (j = 0; j < 2*i+1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+
+	}
+
+	for (i = 0; i < k - 1; i++)
+	{
+		int j = 0;
+		for (j = 0; j <= i; j++)
+		{
+			printf(" ");
+		}
+		for (j = 0; j <2*(k-1-i)-1 ; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+}
 int main()
 {
-	char* c[] = { "ENTER","NEW","POINT","FIRST" };
-	char** cp[] = { c + 3,c + 2,c + 1,c };
-	char*** cpp = cp;
-	printf("%s\n", **++cpp);
-	printf("%s\n", *-- * ++cpp + 3);  //printf()里面的表达式++运算结果保留 c上一个cpp的变化会保留
-	printf("%s\n", *cpp[-2] + 3);  //cpp[-2]==*(cpp+(-2))
-	printf("%s\n", cpp[-1][-1] + 1);
+	int line = 0;
+	scanf("%d", &line);
+	Print(line);
 	system("pause");
 	return 0;
 }
+//     *//5
+//    ***//4
+//   *****
+//  *******
+// *********
+//***********
+// *********  //1
+//  *******   //2
+//   *****   //3
+//    ***
+//     *
