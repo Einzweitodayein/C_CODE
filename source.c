a6900e8558e94a2887c7eb58f9373d1015edaafe
ein <hello@qq.com>
2023-02-17 10:32:05 +0800

    实现函数memcpy

--- source.c
+++ source.c
@@ -2,27 +2,36 @@
 #include<stdio.h>
 #include<string.h>
 #include<errno.h>
+#include<assert.h>
+
+struct S
+{
+	char name[20];
+	int age;
+};
+
+void* my_memcpy(void* dest, const void* src, size_t num)
+{
+	void* ret = dest;
+	assert(dest != NULL);
+	assert(src != NULL);
+
+	while (num--)
+	{
+		*(char*)dest = *(char*)src;
+		++(char*)dest;   //优先级后置++ > 强制类型转换 > 前置++   所以采用前置++
+		++(char*)src;
+	}
+	return ret;
+}
 int main()
 {
-	//0---No error
-	//1---Operation not permitted  不允许操作
-	//2---No such file or directory 没有这样的文件或文件夹
-	//3---No such process  没有这样的进程
-	//....
-	// char* str = strerror(1);
-	//当c语言的库函数在执行过程中，发生了错误，就会把对应的错误码，赋值到errno中
-	//errno是一个全局的错误码的变量
-
-	FILE* pf = fopen("test.txt", "r");
-	if (pf == NULL)
-	{
-		printf("%s\n", strerror(errno));
-	}
-	else
-	{
-		printf("open file success\n");
-	}
-	
+	int arr1[] = { 1,2,3,4,5 };
+	int arr2[5] = { 0 };
+	struct S arr3[] = { {"张三",20},{"李四",30} };
+	struct S arr4[3] = { 0 };
+	memcpy(arr2,arr1,sizeof(arr1));
+	my_memcpy(arr4, arr3, sizeof(arr3));
 	system("pause");
 	return 0;
 }
