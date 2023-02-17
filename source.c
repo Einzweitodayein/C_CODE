812dc925439707de826869247913a9f8044a15b7
ein <hello@qq.com>
2023-02-17 09:26:26 +0800

    错误信息报告

--- source.c
+++ source.c
@@ -1,33 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-#include<assert.h>
 #include<string.h>
-
+#include<errno.h>
 int main()
 {
-	char arr[] = "zpw@bitedu.tech";
-	char* p = "@.";
+	//0---No error
+	//1---Operation not permitted  不允许操作
+	//2---No such file or directory 没有这样的文件或文件夹
+	//3---No such process  没有这样的进程
+	//....
+	// char* str = strerror(1);
+	//当c语言的库函数在执行过程中，发生了错误，就会把对应的错误码，赋值到errno中
+	//errno是一个全局的错误码的变量
 
-	char buf[1024] = { 0 };
-	strcpy(buf, arr);
-
-	//切割buf中的字符串
-	
-	//写法不好
-	//char* ret = strtok(arr, p);
-	//printf("%s\n", ret); //zpw
-
-	//ret = strtok(NULL, p);
-	//printf("%s\n", ret); //bitedu
-
-	//ret = strtok(NULL, p);
-	//printf("%s\n", ret); //tech
-
-	char* ret = NULL;
-	for (ret = strtok(arr, p); ret != NULL; ret = strtok(NULL, p))
+	FILE* pf = fopen("test.txt", "r");
+	if (pf == NULL)
 	{
-		printf("%s\n", ret);
+		printf("%s\n", strerror(errno));
 	}
+	else
+	{
+		printf("open file success\n");
+	}
+	
 	system("pause");
 	return 0;
 }
